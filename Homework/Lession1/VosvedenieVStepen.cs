ee0d491a10b4136e8133c32e2d25255e56aed179
Никита Черный <tetkaxa@gmail.com>
2021-04-01 23:14:46 +0300

    First commit

---
+++ Homework/Lession1/VosvedenieVStepen.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VozvedenieVStepen2
+{
+    public class VosvedenieVStepen
+    {
+        static double Getpow(double n, double p)
+        {
+            return Math.Exp(Math.Log(n) * p);
+        }
+
+        public void Run()
+        {
+            // Читает введёное любое (включая с точкой) число пользователя.
+            // Если он ввёл НЕ число то пользователь должен быть уведомлен
+            // получив сообщение "Повторите ввод"
+            // Цель - пользователь вводит 2-а числа, а программа
+            // Возводит в степень число вызывая функцию GetPow
+            // В которой нельзя использовать математическую функцию возведения
+            // в степень (Нельзя использовать Math.Pow) необходимо самому реализовать
+            // метод возведения в степень
+            for (; ; )
+            {
+                Console.WriteLine("Введите число: ");
+                string number = Console.ReadLine();
+                double n;
+                if (Double.TryParse(number, out n))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Введите степень: ");
+                    string power = Console.ReadLine();
+                    double p;
+                    if (Double.TryParse(power, out p))
+                    {
+                        double result = Getpow(n, p);
+                        Console.WriteLine();
+                        Console.WriteLine($"Число {number} в степени {power} равно {result}");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Введенное значение не является числом");
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Введенное значение не является числом");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VozvedenieVStepen2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vosvedenieVStepen = new VosvedenieVStepen();
+            vosvedenieVStepen.Run();
+            Console.ReadKey();
+        }
+    }
+}
