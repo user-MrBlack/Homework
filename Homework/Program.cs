5b2ea13656afc7da42a1fcfedbea28166494e463
Никита Черный <tetkaxa@gmail.com>
2021-04-01 23:20:28 +0300

    Добавил тест комментарий

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -6,6 +6,7 @@ namespace VozvedenieVStepen2
     {
         static void Main(string[] args)
         {
+            // Test
             var vosvedenieVStepen = new VosvedenieVStepen();
             vosvedenieVStepen.Run();
             Console.ReadKey();
