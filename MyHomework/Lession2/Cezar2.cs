824534edf04dc7ca13ee32b0ccaf86faccaebc8e
Никита Черный <tetkaxa@gmail.com>
2021-07-03 19:18:22 +0300

    Внесены исправления: - Выполнено выравнивание кода; - Добавлена возмодность использование ключа больших значений; - Вынос констант за пределы метода;

--- MyHomework/Lession2/Cezar2.cs
+++ MyHomework/Lession2/Cezar2.cs
@@ -6,21 +6,24 @@ namespace HomeWork.Lession2
 {
     public class Cezar2
     {
+        public static int startLowerChar = 'a';
+        public static int endLowerChar = 'z';
+        public static int startUpperChar = 'A';
+        public static int endUpperChar = 'Z';
         public static void Run()
         {
             for (; ; )
             {
-                // > "HI"
-                //Console.WriteLine("\\\"Строка\\\"\0");
-                // \"Строка\"
-
                 Console.Write("Введите текст :");
                 string text = Console.ReadLine();
                 Console.WriteLine("Выберите число где 1-Шифрование и 2-Дешифрование");
                 var oper = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Введите ключ");
                 int key = Convert.ToInt32(Console.ReadLine());
-                
+                if (key>=26)
+                {
+                    key = key % 26;
+                }
                 if (oper == 1)
                 {
                     Console.WriteLine(Encrypt(text, key));
@@ -36,11 +39,6 @@ namespace HomeWork.Lession2
 
         public static string Decrypt(string text, int key) // Дешифровка
         {
-            int startLowerChar = 'a';
-            int endLowerChar = 'z';
-            int startUpperChar = 'A';
-            int endUpperChar = 'Z';
-
             char[] buffer = new char[text.Length];
 
             for (int i = 0; i < text.Length; i++)
@@ -70,27 +68,14 @@ namespace HomeWork.Lession2
 
         public static string Encrypt(string text, int key) // Шифрование
         {  
-            // a = 10
-            // z = 30
-            // z + 1 = ?
-
-            // x [28] + 5 = b ?
-            // 
-            // abcdef.....xyz[]
-            // text = hello z
-            int startLowerChar = 'a';
-            int endLowerChar = 'z';
-            int startUpperChar = 'A';
-            int endUpperChar = 'Z';
-
             char[] buffer = new char[text.Length];
             
             for (int i = 0; i < text.Length; i++)
             {
                 int symbol = text[i];
                 if (symbol != ' ')
-                    {
-                        int endChar = endLowerChar, startChar = startLowerChar;
+                {
+                    int endChar = endLowerChar, startChar = startLowerChar;
                     if (char.IsUpper(text[i]))
                     {
                         endChar = endUpperChar;
@@ -102,7 +87,8 @@ namespace HomeWork.Lession2
                     {
                         symbol = startChar + (symbol - endChar) - 1;
                     }
-            }
+                }
+
                 buffer[i] = (char)symbol;
             }
             return new string(buffer);
@@ -110,24 +96,24 @@ namespace HomeWork.Lession2
         
     }
 }
-//       "Old metod Encrypt"
-//if (text[i] == text.ToUpper()[i])
-//{
-//    symbol = symbol + key;
-//
-//    if (symbol > endUpperChar)
-//    {
-//        symbol = startUpperChar + (symbol - endUpperChar) - 1;
-//    }
-//}
-//else
-//{
-//    symbol = symbol + key;
-//    if (symbol > endLowerChar)
-//    {
-//        // a [10] x [28] z [30]   x + 5 = 33
-//        // 10 + (33 - 30)
-//        symbol = startLowerChar + (symbol - endLowerChar) - 1;
-//    }
-//}
-//    buffer[i] = (char)symbol;
+            //       "Old metod Encrypt"
+            //if (text[i] == text.ToUpper()[i])
+            //{
+            //    symbol = symbol + key;
+            //
+            //    if (symbol > endUpperChar)
+            //    {
+            //        symbol = startUpperChar + (symbol - endUpperChar) - 1;
+            //    }
+            //}
+            //else
+            //{
+            //    symbol = symbol + key;
+            //    if (symbol > endLowerChar)
+            //    {
+            //        // a [10] x [28] z [30]   x + 5 = 33
+            //        // 10 + (33 - 30)
+            //        symbol = startLowerChar + (symbol - endLowerChar) - 1;
+            //    }
+            //}
+            //    buffer[i] = (char)symbol;
